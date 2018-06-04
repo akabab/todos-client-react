799c5b0f90975d7ffb4aab05703f7b2f812b8709
Clement Denis <le.mikmac@gmail.com>
2018-06-04 15:16:43 +0200

    load todos action and reducer

--- src/store.js
+++ src/store.js
@@ -5,8 +5,20 @@ const initialState = {
 }
 
 const reducer = (state, action) => {
+
+  if (action.type === 'LOAD_TODOS') {
+    return {
+      todos: action.todos
+    }
+  }
+
   return state
 }
 
 export const store = createStore(reducer, initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+export const actions = {
+  loadTodos: todos => store.dispatch({ type: 'LOAD_TODOS', todos })
+}
+
