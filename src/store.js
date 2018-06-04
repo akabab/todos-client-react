e5ebe197454e9f3010d3c61207cc8cbc2e0ffbe1
Clement Denis <le.mikmac@gmail.com>
2018-06-04 15:15:53 +0200

    export store and bind redux devtool extension

--- src/store.js
+++ src/store.js
@@ -7,3 +7,6 @@ const initialState = {
 const reducer = (state, action) => {
   return state
 }
+
+export const store = createStore(reducer, initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
